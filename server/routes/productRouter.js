047dbf1549dc021c3ccef2473b570d8e609d475b
chiemelaumeh <chiemelaumeh1@gmail.com>
2023-11-17 13:38:03 -0600

    ad dded nodejs error handler

---
+++ server/routes/productRouter.js
@@ -0,0 +1,6 @@
+import express from "express";
+const productRouter = express.Router();
+
+productRouter.post("/createproduct")
+
+export default productRouter
--- server/server.js
+++ server/server.js
@@ -3,16 +3,30 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
-
 const port = process.env.PORT || 8800;
 
-import { dbConnection } from './db.js';
+import productRouter from './routes/productRouter.js';
 
+import { dbConnection } from './db.js';
+app.use('/api/products', productRouter);
+
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.errorMessage || 'Something went wrong';
+  res.status(errorStatus).json({
+    successful: false,
+    status: error.Status,
+    message: error.message,
+    stack: err.stack,
+  });
+
+});
 app.use('/', (req, res) => {
   res.status(200).send('Api is working');
 });
 
-mongoose.connection.on('disconnectes', () => {
+mongoose.connection.on('disconnected', () => {
   console.log('...Database disconnected');
 });
 app.listen(port, () => {
