20de9461a5a2fd6d700c2211300461bc3d4071a3
chiemelaumeh <chiemelaumeh1@gmail.com>
2023-11-16 22:47:54 -0600

    connected mongodb

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
-import express from "express"
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
 const port = process.env.PORT || 8800;
 
+import { dbConnection } from './db.js';
 
+app.use('/', (req, res) => {
+  res.status(200).send('Api is working');
+});
 
-app.use("/", (req, res) => {
-  res.status(200).send("Api is working")
-})
-
+mongoose.connection.on('disconnectes', () => {
+  console.log('...Database disconnected');
+});
 app.listen(port, () => {
-  console.log(`...Listening on port ${port}`)
-})
+  dbConnection();
+  console.log(`...Listening on port ${port}`);
+});
