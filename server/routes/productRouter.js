b2639d94022a9c5ccff4747825aacc2f42ad9003
chiemelaumeh <chiemelaumeh1@gmail.com>
2023-12-16 00:12:26 -0600

    added dummy data

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { createProduct } from '../controllers/products/createproduct.js';
 import { fetchFeatured } from '../controllers/products/fetchfeatured.js';
-
+import { deleteProducts } from '../controllers/products/deleteProduct.js';
 const productRouter = express.Router();
 
 productRouter.post('/createproduct', createProduct);
+productRouter.delete('/deleteproducts', deleteProducts)
 productRouter.get('/fetchfeatured', fetchFeatured);
 
 export default productRouter;
